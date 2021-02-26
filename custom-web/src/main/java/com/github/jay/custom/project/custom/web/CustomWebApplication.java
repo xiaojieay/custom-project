f37e75297061f97666f9ac1e3df7938f130aac50
xiaojie <3332913700@qq.com>
2021-02-26 17:09:19 +0800

    添加启动时日志处理

--- custom-web/src/main/java/com/github/jay/custom/project/custom/web/CustomWebApplication.java
+++ custom-web/src/main/java/com/github/jay/custom/project/custom/web/CustomWebApplication.java
@@ -3,10 +3,11 @@ package com.github.jay.custom.project.custom.web;
 import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
 import com.github.jay.custom.project.custom.common.constant.SystemConstant;
 import com.github.jay.custom.project.custom.web.util.SystemConfigCenterUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -21,7 +22,9 @@ import java.util.Properties;
 @SpringBootApplication(scanBasePackages = "com.github.jay.custom.project",
     exclude = {DataSourceAutoConfiguration.class})
 @EnableNacosDiscovery
-public class CustomWebApplication extends SpringBootServletInitializer {
+public class CustomWebApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomWebApplication.class);
 
     public static void main(String[] args) {
         Properties properties = new Properties();
@@ -30,6 +33,7 @@ public class CustomWebApplication extends SpringBootServletInitializer {
             System.getProperties().putAll(properties);
             SystemConfigCenterUtils.initConfig();
         } catch (Exception e) {
+            logger.error("初始化系统级配置失败");
             e.printStackTrace();
         }
         SpringApplication.run(CustomWebApplication.class, args);
